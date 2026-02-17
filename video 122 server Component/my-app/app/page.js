6a464f6660a629505a428ea04cadd75b9016b003
Gaurav <teachnicalgaurav45@gmail.com>
2026-02-17 23:32:11 +0530

    next with me

---
+++ video 122 server Component/my-app/app/page.js
@@ -0,0 +1,17 @@
+// "use client"
+import { useState } from "react";
+
+export default function Home() {
+
+    // const [count, setcount] = useState(0)
+    console.log("Hey i am Gaurav")
+
+  return (
+    <div>
+      i am a component
+       {/* {count}
+      <br />
+      <button onClick={()=>setcount(count + 1)}>change me</button> */}
+    </div>
+  );
+}
